1bfe914ab15d0874cc33430344a0fd0d6c3d58ff
Charles Adriano Costa da Silva <charlesadriano@gmail.com>
2023-03-01 20:31:21 -0300

    acrescentado a memôria do resuldado, mas a memôria ainda não detecta a origem da opração.

--- calculadorapergunta.py
+++ calculadorapergunta.py
@@ -1,6 +1,7 @@
 #Calculadora que pergunta;
 
 continuar = "s"
+armazena = [0,1,2,3,4,5,6,7,8]
 
 while continuar == "s" or continuar == "S":
     print("Entre com os parâmetros solicitados.")
@@ -9,15 +10,24 @@ while continuar == "s" or continuar == "S":
     n1 = float(input("Digite o valor correspondente ao segundo dado da operação: "))
     if operacao == "+":
         resultado = n + n1
+        armazena[0] = resultado
         print(resultado)
+
     if operacao == "-":
         resultado = n - n1
+        armazena[1] = resultado
         print(resultado)
+
     if operacao == "*":
         resultado = n * n1
+        armazena[2] = resultado
         print(resultado)
+
     if operacao == "/":
         resultado = n / n1
+        aramazena[3] = resultado
         print(resultado)
+
     continuar = input("Deseja fazer alguma outra operação? [n] não, [s]sim ")
-    
+print("O resultado das oprações foram", armazena[1], "resultado da soma, ", armazena[0], "resultado da subtração",
+armazena[2], "resultado da multiplicação e ", armazena[4], "resultado da divisão")
